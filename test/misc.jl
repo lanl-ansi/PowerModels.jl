4bcdeda9b9bc73aee128aeb7bc9a5b1fc1454b4a
Carleton <cjc@lanl.gov>
2016-08-24 17:50:54 -0600

    moving solver configs to runtests

--- test/misc.jl
+++ test/misc.jl
@@ -2,21 +2,21 @@
 
 @testset "test ac api" begin
     @testset "3-bus case" begin
-        result = run_api_opf("../test/data/case3.json", APIACPPowerModel, IpoptSolver(tol=1e-6, print_level=0))
+        result = run_api_opf("../test/data/case3.json", APIACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
         @test isapprox(result["objective"], 1.3375; atol = 1e-3)
         @test isapprox(result["solution"]["bus"][1]["pd"], 147.1; atol = 1e0)
     end
     @testset "5-bus pjm case" begin
-        result = run_api_opf("../test/data/case5.json", APIACPPowerModel, IpoptSolver(tol=1e-6, print_level=0))
+        result = run_api_opf("../test/data/case5.json", APIACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
         @test isapprox(result["objective"], 2.6885; atol = 1e-3)
         @test isapprox(result["solution"]["bus"][4]["pd"], 1075.4; atol = 1e0)
     end
     @testset "30-bus ieee case" begin
-        result = run_api_opf("../test/data/case30.json", APIACPPowerModel, IpoptSolver(tol=1e-6, print_level=0))
+        result = run_api_opf("../test/data/case30.json", APIACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
         @test isapprox(result["objective"], 1.6632; atol = 1e-3)
@@ -27,19 +27,19 @@ end
 
 @testset "test ac sad" begin
     @testset "3-bus case" begin
-        result = run_sad_opf("../test/data/case3.json", ACPPowerModel, IpoptSolver(tol=1e-6, print_level=0))
+        result = run_sad_opf("../test/data/case3.json", ACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
         @test isapprox(result["objective"], 0.3144; atol = 1e-2)
     end
     @testset "5-bus pjm case" begin
-        result = run_sad_opf("../test/data/case5.json", ACPPowerModel, IpoptSolver(tol=1e-6, print_level=0))
+        result = run_sad_opf("../test/data/case5.json", ACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
         @test isapprox(result["objective"], 0.02233; atol = 1e-2)
     end
     @testset "30-bus ieee case" begin
-        result = run_sad_opf("../test/data/case30.json", ACPPowerModel, IpoptSolver(tol=1e-6, print_level=0))
+        result = run_sad_opf("../test/data/case30.json", ACPPowerModel, ipopt_solver)
 
         @test result["status"] == :LocalOptimal
         @test isapprox(result["objective"], 0.1537; atol = 1e-2)
--- test/runtests.jl
+++ test/runtests.jl
@@ -18,8 +18,10 @@ else
     const Test = BaseTestNext
 end
 
-
-pajarito_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=IpoptSolver(tol=1e-6, print_level=0), log_level=0)
+# default setup for solvers
+ipopt_solver = IpoptSolver(tol=1e-6, print_level=0)
+pajarito_solver = PajaritoSolver(mip_solver=GLPKSolverMIP(), cont_solver=ipopt_solver, log_level=0)
+scs_solver = SCSSolver(max_iters=1000000, verbose=0)
 
 include("output.jl")
 
