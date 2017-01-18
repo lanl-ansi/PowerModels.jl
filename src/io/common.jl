0bbe0ab69f89a2ecd36ebf0e0c1aab1aafca1856
Carleton <cjc@lanl.gov>
2017-01-18 15:49:40 -0700

    adding fix to refresh derived values when loading json data, closes #64

--- src/io/common.jl
+++ src/io/common.jl
@@ -2,6 +2,8 @@ function parse_file(file)
     if endswith(file, ".m")
         return PowerModels.parse_matpower(file)
     else
-        return PowerModels.parse_json(file)
+        network_data = PowerModels.parse_json(file)
+        update_derived_values(network_data)
+        return network_data
     end
 end
