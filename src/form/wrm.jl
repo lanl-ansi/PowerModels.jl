6cf939f44a1bfe502d866ec9618d4c398928ff23
ccoffrin <carleton@coffrin.com>
2018-03-07 19:40:36 -0500

    fixing sdp-pf and removing dead code

--- src/form/wrm.jl
+++ src/form/wrm.jl
@@ -122,59 +122,6 @@ function constraint_voltage_angle_difference{T <: AbstractWRMForm}(pm::GenericPo
     cut_complex_product_and_angle_difference(pm.model, w_fr, w_to, wr, wi, angmin, angmax)
 end
 
-#=
-
-""
-function constraint_kcl_shunt{T <: AbstractWRMForm}(pm::GenericPowerModel{T}, n::Int, i, bus_arcs, bus_arcs_dc, bus_gens, pd, qd, gs, bs)
-    w_index = pm.ext[:nw][n][:lookup_w_index][i]
-    w = pm.var[:nw][n][:WR][w_index, w_index]
-
-    p = pm.var[:nw][n][:p]
-    q = pm.var[:nw][n][:q]
-    pg = pm.var[:nw][n][:pg]
-    qg = pm.var[:nw][n][:qg]
-    p_dc = pm.var[:nw][n][:p_dc]
-    q_dc = pm.var[:nw][n][:q_dc]
-
-    @constraint(pm.model, sum(p[a] for a in bus_arcs) + sum(p_dc[a_dc] for a_dc in bus_arcs_dc) == sum(pg[g] for g in bus_gens) - pd - gs*w)
-    @constraint(pm.model, sum(q[a] for a in bus_arcs) + sum(q_dc[a_dc] for a_dc in bus_arcs_dc) == sum(qg[g] for g in bus_gens) - qd + bs*w)
-end
-
-"Creates Ohms constraints (yt post fix indicates that Y and T values are in rectangular form)"
-function constraint_ohms_yt_from{T <: AbstractWRMForm}(pm::GenericPowerModel{T}, n::Int, f_bus, t_bus, f_idx, t_idx, g, b, c, tr, ti, tm)
-    p_fr = pm.var[:nw][n][:p][f_idx]
-    q_fr = pm.var[:nw][n][:q][f_idx]
-
-    w_fr_index = pm.ext[:nw][n][:lookup_w_index][f_bus]
-    w_to_index = pm.ext[:nw][n][:lookup_w_index][t_bus]
-
-    w_fr = pm.var[:nw][n][:WR][w_fr_index, w_fr_index]
-    w_to = pm.var[:nw][n][:WR][w_to_index, w_to_index]
-    wr   = pm.var[:nw][n][:WR][w_fr_index, w_to_index]
-    wi   = pm.var[:nw][n][:WI][w_fr_index, w_to_index]
-
-    @constraint(pm.model, p_fr == g/tm*w_fr + (-g*tr+b*ti)/tm*(wr) + (-b*tr-g*ti)/tm*( wi) )
-    @constraint(pm.model, q_fr == -(b+c/2)/tm*w_fr - (-b*tr-g*ti)/tm*(wr) + (-g*tr+b*ti)/tm*( wi) )
-end
-
-""
-function constraint_ohms_yt_to{T <: AbstractWRMForm}(pm::GenericPowerModel{T}, n::Int, f_bus, t_bus, f_idx, t_idx, g, b, c, tr, ti, tm)
-    q_to = pm.var[:nw][n][:q][t_idx]
-    p_to = pm.var[:nw][n][:p][t_idx]
-
-    w_fr_index = pm.ext[:nw][n][:lookup_w_index][f_bus]
-    w_to_index = pm.ext[:nw][n][:lookup_w_index][t_bus]
-
-    w_fr = pm.var[:nw][n][:WR][w_fr_index, w_fr_index]
-    w_to = pm.var[:nw][n][:WR][w_to_index, w_to_index]
-    wr   = pm.var[:nw][n][:WR][w_fr_index, w_to_index]
-    wi   = pm.var[:nw][n][:WI][w_fr_index, w_to_index]
-
-    @constraint(pm.model, p_to ==    g*w_to + (-g*tr-b*ti)/tm*(wr) + (-b*tr+g*ti)/tm*(-wi) )
-    @constraint(pm.model, q_to ==    -(b+c/2)*w_to - (-b*tr+g*ti)/tm*(wr) + (-g*tr-b*ti)/tm*(-wi) )
-end
-
-
 
 ""
 function add_bus_voltage_setpoint{T <: AbstractWRMForm}(sol, pm::GenericPowerModel{T})
@@ -184,11 +131,3 @@ function add_bus_voltage_setpoint{T <: AbstractWRMForm}(sol, pm::GenericPowerMod
     #add_setpoint(sol, pm, "bus", "va", :va; default_value = 0)
 end
 
-""
-function constraint_voltage_magnitude_setpoint{T <: AbstractWRMForm}(pm::GenericPowerModel{T}, n::Int, i, vm)
-    w_index = pm.ext[:nw][n][:lookup_w_index][i]
-    w = pm.var[:nw][n][:WR][w_index, w_index]
-
-    @constraint(pm.model, w == vm^2)
-end
-=#
