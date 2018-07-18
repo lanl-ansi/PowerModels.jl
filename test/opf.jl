fb86fa8560131fbb4dc11662b7ce9b8d45f200f0
Jonas Kersulis <kersulis@umich.edu>
2018-07-18 12:35:44 -0500

    add tests (apply existing SDP tests to SDPDecompForm)

--- test/opf.jl
+++ test/opf.jl
@@ -488,7 +488,52 @@ end
     #end
 end
 
+@testset "test sdp opf with constraint decomposition" begin
+    @testset "3-bus case" begin
+        result = run_opf("../test/data/matpower/case3.m", SDPDecompPowerModel, scs_solver)
 
+        @test result["status"] == :Optimal
+        @test isapprox(result["objective"], 5851.3; atol = 1e0)
+    end
+    # TODO see if convergence time can be improved
+    #@testset "5-bus asymmetric case" begin
+    #    result = run_opf("../test/data/matpower/case5_asym.m", SDPDecompPowerModel, scs_solver)
+
+    #    @test result["status"] == :Optimal
+    #    @test isapprox(result["objective"], 16664; atol = 1e0)
+    #end
+    #@testset "5-bus gap case" begin
+    #    result = run_opf("../test/data/matpower/case5_gap.m", SDPDecompPowerModel, scs_solver)
+
+    #    @test result["status"] == :Optimal
+    #    @test isapprox(result["objective"], TBD; atol = 1e0)
+    #end
+    @testset "5-bus with asymmetric line charge" begin
+        result = run_opf("../test/data/pti/case5_alc.raw", SDPDecompPowerModel, scs_solver)
+
+        @test result["status"] == :Optimal
+        @test isapprox(result["objective"], 1005.31; atol = 1e-1)
+    end
+    @testset "14-bus case" begin
+        result = run_opf("../test/data/matpower/case14.m", SDPDecompPowerModel, scs_solver)
+
+        @test result["status"] == :Optimal
+        @test isapprox(result["objective"], 8079.97; atol = 1e0)
+    end
+    @testset "6-bus case" begin
+        result = run_opf("../test/data/matpower/case6.m", SDPDecompPowerModel, scs_solver)
+
+        @test result["status"] == :Optimal
+        @test isapprox(result["objective"], 11558.5; atol = 1e0)
+    end
+    # TODO replace this with smaller case, way too slow for unit testing
+    #@testset "24-bus rts case" begin
+    #    result = run_opf("../test/data/matpower/case24.m", SDPDecompPowerModel, scs_solver)
+
+    #    @test result["status"] == :Optimal
+    #    @test isapprox(result["objective"], 75153; atol = 1e0)
+    #end
+end
 
 
 @testset "test ac v+t polar opf" begin
