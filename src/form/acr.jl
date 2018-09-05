d23c85d0a8acf83085c84beae2782d67fb5c7fe7
Carleton Coffrin <cjc@lanl.gov>
2018-09-05 11:28:34 -0600

    adding indentation to ACR doc string

--- src/form/acr.jl
+++ src/form/acr.jl
@@ -14,11 +14,11 @@ AC power flow formulation with rectangular bus voltage variables.
 
 ```
 @techreport{Cain2012,
-author = {Cain, Mary B and {O' Neill}, Richard P and Castillo, Anya},
-pages = {1--36},
-title = {{History of optimal power flow and formulations}},
-url = {https://www.ferc.gov/industries/electric/indus-act/market-planning/opf-papers/acopf-1-history-formulation-testing.pdf}
-year = {2012}
+  author = {Cain, Mary B and {O' Neill}, Richard P and Castillo, Anya},
+  pages = {1--36},
+  title = {{History of optimal power flow and formulations}},
+  url = {https://www.ferc.gov/industries/electric/indus-act/market-planning/opf-papers/acopf-1-history-formulation-testing.pdf}
+  year = {2012}
 }
 ```
 """
