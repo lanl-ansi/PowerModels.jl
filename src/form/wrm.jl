fcb9d4508c03b3c3aaf2a9230be5c997e7fc409b
Jonas Kersulis <kersulis@users.noreply.github.com>
2018-09-07 16:47:35 -0400

    Citations for sparsity-exploiting SDP OPF (#385)

--- src/form/wrm.jl
+++ src/form/wrm.jl
@@ -145,7 +145,39 @@ end
 
 abstract type SparseSDPWRMForm <: SDPWRMForm end
 
-""
+"""
+Sparsity-exploiting semidefinite relaxation of AC OPF
+
+Proposed in:
+```
+@article{doi:10.1137/S1052623400366218,
+author = {Fukuda, M. and Kojima, M. and Murota, K. and Nakata, K.},
+title = {Exploiting Sparsity in Semidefinite Programming via Matrix Completion I: General Framework},
+journal = {SIAM Journal on Optimization},
+volume = {11},
+number = {3},
+pages = {647-674},
+year = {2001},
+doi = {10.1137/S1052623400366218},
+URL = {https://doi.org/10.1137/S1052623400366218},
+eprint = {https://doi.org/10.1137/S1052623400366218}
+}
+```
+Original application to OPF by:
+```
+@ARTICLE{6064917,
+author={R. A. Jabr},
+title={Exploiting Sparsity in SDP Relaxations of the OPF Problem},
+journal={IEEE Transactions on Power Systems},
+volume={27},
+number={2},
+pages={1138-1139},
+year={2012},
+month={May}},
+doi={10.1109/TPWRS.2011.2170772},
+ISSN={0885-8950}
+```
+"""
 const SparseSDPWRMPowerModel = GenericPowerModel{SparseSDPWRMForm}
 
 ""
@@ -507,4 +539,3 @@ function problem_size(groups)
     A = prim(overlap_graph(groups))
     return sum(nvars.(Int64.(nonzeros(A)))) + sum(nvars.(length.(groups)))
 end
-
