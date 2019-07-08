faea7ab463595b12e3eab141b2f788d50878bf9a
Noah Rhodes <noahx23@gmail.com>
2019-07-08 17:16:32 -0600

    Change solver for DC MILP to cbc from juniper
    updates to case5_strg removed nonlinear objective and
    allow MILP solvers.  The update makes it impossible to
    for the run_uc_opf DC formulation to disable a storage
    device.

--- test/opf-var.jl
+++ test/opf-var.jl
@@ -183,17 +183,6 @@ end
         end
     end
 
-    @testset "test dc opf" begin
-        @testset "5-bus uc case" begin
-            result = PowerModels._run_uc_opf("../test/data/matpower/case5_uc_strg.m", DCPPowerModel, juniper_solver)
-
-            @test result["termination_status"] == LOCALLY_SOLVED
-            @test isapprox(result["objective"], 17073.2; atol = 1e0)
-            @test isapprox(result["solution"]["storage"]["1"]["status"], 1.0, atol=1e-6)
-            @test isapprox(result["solution"]["storage"]["2"]["status"], 0.0, atol=1e-6)
-        end
-    end
-
 end
 
 
@@ -244,9 +233,9 @@ end
 
     @testset "test mi dc opf" begin
         @testset "5-bus case" begin
-            result = PowerModels._run_strg_mi_opf("../test/data/matpower/case5_strg.m", PowerModels.DCPPowerModel, juniper_solver)
+            result = PowerModels._run_strg_mi_opf("../test/data/matpower/case5_strg.m", PowerModels.DCPPowerModel, cbc_solver)
 
-            @test result["termination_status"] == LOCALLY_SOLVED
+            @test result["termination_status"] == OPTIMAL
             @test isapprox(result["objective"], 16840.7; atol = 1e0)
 
             @test isapprox(result["solution"]["storage"]["1"]["se"],  0.0; atol = 1e0)
