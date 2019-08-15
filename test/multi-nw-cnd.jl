06f21ffd32c4a2afbef5f4c7810e236f233eeb65
ccoffrin <carleton@coffrin.com>
2019-08-15 08:27:43 -0600

    remove unstable linux test, mn-mc-dc-nlp

--- test/multi-nw-cnd.jl
+++ test/multi-nw-cnd.jl
@@ -231,12 +231,15 @@
             @test isapprox(result["objective"], 1.59445e5; atol = 1e2)
         end
 
+        #=
+        # numerical error on linux
         @testset "dc 5-bus storage case" begin
             result = PowerModels._run_mn_mc_strg_opf(mn_mc_data, PowerModels.DCPPowerModel, ipopt_solver)
 
             @test result["termination_status"] == LOCALLY_SOLVED
             @test isapprox(result["objective"], 1.57804e5; atol = 1e2)
         end
+        =#
 
         #=
         # base case not yet implemented
