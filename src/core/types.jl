01c7101b96294a5dc5f40720eb5e3d18e7c91634
Carleton Coffrin <cjc@lanl.gov>
2019-10-14 09:18:41 -0600

    Added note on susceptance values in DCPPowerModel docs

--- src/core/types.jl
+++ src/core/types.jl
@@ -129,6 +129,14 @@ abstract type AbstractDCPModel <: AbstractActivePowerModel end
 """
 Linearized 'DC' power flow Model with polar voltage variables.
 
+The this model is a basic linear active-power-only approximation, which uses branch susceptance values 
+`br_b = -br_x / (br_x^2 + br_x^2)` for determining the network phase angles.  Furthermore, transformer
+parameters such as tap ratios and phase shifts are not considered as part of this model.
+
+It is important to note that it is also common for active-power-only approximations to use `1/br_x` for 
+determining the network phase angles, instead of the `br_b` value that is used here.  Small discrepancies
+in solutions should be expected when comparing active-power-only approximations across multiple tools.
+
 ```
 @ARTICLE{4956966,
   author={B. Stott and J. Jardim and O. Alsac},
