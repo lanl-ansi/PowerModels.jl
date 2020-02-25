bd1f54949076b08a999c08efa66e4e0d77ec3396
ccoffrin <carleton@coffrin.com>
2020-02-24 22:51:20 -0700

    fix doc string in IVRPowerModel

--- src/core/types.jl
+++ src/core/types.jl
@@ -126,12 +126,11 @@ abstract type AbstractIVRModel <: AbstractACRModel end
 
 """
 Current voltage formulation of AC OPF. The formulation uses rectangular
-coordinates for both current and voltage.
-Note that, even though Kirchhoff's circuit laws are linear in current and
-voltage, this formulation is nonconvex due to constants power loads/generators
-and apparent power limits.
+coordinates for both current and voltage.  Note that, even though Kirchhoff's
+circuit laws are linear in current and voltage, this formulation is nonconvex
+due to constants power loads/generators and apparent power limits.
 
-
+```
 @techreport{ONeill2012,
     author = {{O' Neill}, Richard P and Castillo, Anya and Cain, Mary B},
     pages = {1--18},
