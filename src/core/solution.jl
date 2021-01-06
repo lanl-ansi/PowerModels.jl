67a43a9ecba407736125ec82491e13ab87f9ec79
Byron Tasseff <btasseff@lanl.gov>
2021-01-06 11:42:05 -0700

    Modify argument to helper functions.

--- src/core/solution.jl
+++ src/core/solution.jl
@@ -25,12 +25,12 @@ end
 
 
 "PowerModels wrapper for the InfrastructureModels `sol_component_value` function."
-function sol_component_value(aim::_IM.AbstractInfrastructureModel, n::Int, comp_name::Symbol, field_name::Symbol, comp_ids, variables)
+function sol_component_value(aim::AbstractPowerModel, n::Int, comp_name::Symbol, field_name::Symbol, comp_ids, variables)
     return _IM.sol_component_value(aim, pm_it_sym, n, comp_name, field_name, comp_ids, variables)
 end
 
 
 "PowerModels wrapper for the InfrastructureModels `sol_component_value_edge` function."
-function sol_component_value_edge(aim::_IM.AbstractInfrastructureModel, n::Int, comp_name::Symbol, field_name_fr::Symbol, field_name_to::Symbol, comp_ids_fr, comp_ids_to, variables)
+function sol_component_value_edge(aim::AbstractPowerModel, n::Int, comp_name::Symbol, field_name_fr::Symbol, field_name_to::Symbol, comp_ids_fr, comp_ids_to, variables)
     return _IM.sol_component_value(aim, pm_it_sym, n, comp_name, field_name_fr, field_name_to, comp_ids_fr, comp_ids_to, variables)
 end
