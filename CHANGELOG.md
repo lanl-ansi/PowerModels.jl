e9f5efac5a1ed86382b91f2bf7665c0c6b1e7df4
Oscar Dowson <odow@users.noreply.github.com>
2025-04-22 11:24:06 +1200

    Fix parse_file to use JSON.parsefile (#958)
    Co-authored-by: LKuhrmann <LKuhrmann@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ PowerModels.jl Change Log
 - Fix dictionary call syntax error in psse export (#941)
 - Updated the logging message when components are deactivated (#943)
 - Improve performance of `calc_basic_incidence_matrix` (#946)
+- Fix `parse_file` to use `JSON.parsefile` (#958)
 
 ### v0.21.3
 - Fix no-buses bug in `calc_connected_components` (#933)
--- src/io/common.jl
+++ src/io/common.jl
@@ -1,31 +1,32 @@
 """
-    parse_file(file; import_all)
+    parse_file(io::String; import_all=false, validate=true)
+    parse_file(io::IO; import_all=false, validate=true, filetype="json")
 
-Parses a Matpower .m `file` or PTI (PSS(R)E-v33) .raw `file` into a
-PowerModels data structure. All fields from PTI files will be imported if
-`import_all` is true (Default: false).
+Parse a file into a PowerModels data structure.
+
+The supported file types are:
+
+ * `.m`: a Matpower file
+ * `.raw`: a PTI (PSS(R)E-v33)
+ * `.json`: a PowerModels.jl JSON file
+
+All fields from PTI files will be imported if `import_all` is true.
+
+If `validate = true`, PowerModels will validate the imported data for correctness.
 """
-function parse_file(file::String; import_all=false, validate=true)
-    pm_data = open(file) do io
-        pm_data = parse_file(io; import_all=import_all, validate=validate, filetype=split(lowercase(file), '.')[end])
+function parse_file(io::Union{IO,String}; import_all=false, validate=true, filetype="json")
+    if io isa String
+        filetype=lowercase(last(split(io, '.')))
     end
-    return pm_data
-end
-
-
-"Parses the iostream from a file"
-function parse_file(io::IO; import_all=false, validate=true, filetype="json")
     if filetype == "m"
-        pm_data = PowerModels.parse_matpower(io, validate=validate)
+        return PowerModels.parse_matpower(io; validate)
     elseif filetype == "raw"
-        pm_data = PowerModels.parse_psse(io; import_all=import_all, validate=validate)
+        return PowerModels.parse_psse(io; import_all, validate)
     elseif filetype == "json"
-        pm_data = PowerModels.parse_json(io; validate=validate)
+        return PowerModels.parse_json(io; validate)
     else
         Memento.error(_LOGGER, "Unrecognized filetype: \".$filetype\", Supported extensions are \".raw\", \".m\" and \".json\"")
     end
-
-    return pm_data
 end
 
 
@@ -63,10 +64,10 @@ end
 Export a PowerModels data structure to the file according of the extension:
     - `.m` : Matpower
     - `.raw` : PTI (PSS(R)E-v33)
-    - `.json` : JSON 
+    - `.json` : JSON
 """
 function export_file(file::AbstractString, data::Dict{String, Any})
-    if occursin(".", file) 
+    if occursin(".", file)
         open(file, "w") do io
             export_file(io, data, filetype=split(lowercase(file), '.')[end])
         end
